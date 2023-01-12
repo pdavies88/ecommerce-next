edb71a15ae39be5c212ec8aa2908068ed7e9d262
Preston Davies <pdavies88@gmail.com>
2023-01-12 10:04:40 -0500

    NextJS Init with ESLint and TS

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'next/core-web-vitals',
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  plugins: [
+    'react'
+  ],
+  rules: {
+    eqeqeq: 'warn',
+    'no-console': 'warn',
+    'react/display-name': 'off',
+    'react/no-children-prop': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  }
+}
